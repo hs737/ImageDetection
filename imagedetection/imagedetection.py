9eb05d24358b7703b5577530e87a9976a24af551
Harsh <hsinha146@gmail.com>
2016-12-27 11:47:32 -0500

    Initial files. Beginning tutorial

---
+++ imagedetection/imagedetection.py
@@ -0,0 +1,4 @@
+
+
+class ImageDetection:
+    pass
---
+++ index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+# import the necessary packages
+from PIL import Image
+import imagehash
+import argparse
+import shelve
+import glob
+
+# construct the argument parse and parse the arguments

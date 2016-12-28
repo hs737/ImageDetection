9060e9dce91d9a9a50246cd3fee82a60c2339b1c
Harsh <hsinha146@gmail.com>
2016-12-28 10:05:16 -0500

    Removed shelve

--- index.py
+++ index.py
@@ -4,19 +4,16 @@
 from PIL import Image
 import imagehash
 import argparse
-import shelve
 import glob
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--dataset", required = True, help = "path to input dataset of images")
-ap.add_argument("-s", "--shelve", required = True, help = "output shelve database")
 ap.add_argument("-q", "--query", required = True, help = "path to the query image")
 args = vars(ap.parse_args())
 print("Arguments: {}".format(args))
 
-# open the shelve database
-db = shelve.open(args["shelve"], writeback = True)
+db = {}
 
 # loop over the image dataset
 for imagePath in glob.glob(args["dataset"] + "/*.jpg"):
@@ -27,7 +24,7 @@ for imagePath in glob.glob(args["dataset"] + "/*.jpg"):
 
     # extract the filename from the path and update the database
     filename = imagePath[imagePath.rfind("/") + 1:]
-    db[h] = db.get(h, []) + [filename]
+    db[h] = (db[h] if h in db else []) + [filename]
 
 # load the query image, compute the difference image hash,
 # and grab the images from the database that have the same hash
@@ -43,5 +40,3 @@ for filename in filenames:
     image = Image.open(args["dataset"] + "/" + filename)
     image.show()
 
-# close the shelve database
-db.close()

0f149f86ebbcd31d853b055f05ad1ed91a032920
Harsh <hsinha146@gmail.com>
2016-12-28 09:27:25 -0500

    Basic code complete. Not stable

--- index.py
+++ index.py
@@ -11,6 +11,7 @@ import glob
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--dataset", required = True, help = "path to input dataset of images")
 ap.add_argument("-s", "--shelve", required = True, help = "output shelve database")
+ap.add_argument("-q", "--query", required = True, help = "path to the query image")
 args = vars(ap.parse_args())
 
 # open the shelve database
@@ -27,4 +28,18 @@ for imagePath in glob.glob(args["dataset"] + "/*.jpg"):
     filename = imagePath[imagePath.rfind("/") + 1:]
     db[h] = db.get(h, []) + [filename]
 
+# load the query image, compute the difference image hash,
+# and grab the images from the database that have the same hash
+# value
+query = Image.open(args["query"])
+h = str(imagehash.dhash(query))
+filenames = db[h] if h in db else []
+print("Found {} images".format(len(filenames)))
+
+# loop over the images
+for filename in filenames:
+    image = Image.open(args["dataset"] + "/" + filename)
+    image.show()
+
+# close the shelve database
 db.close()

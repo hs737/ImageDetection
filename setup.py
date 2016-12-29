3626c18be48b35b34b65196938f40101ec55ec28
Harsh <hsinha146@gmail.com>
2016-12-29 12:12:51 -0500

    Replaced string manipulation of paths with os module

--- setup.py
+++ setup.py
@@ -8,26 +8,21 @@ from setuptools import setup, find_packages
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-
 setup(
     name='Image Search',
     version='0.0.1',
-    # description=('A module that can be imported and used via the command line '
-    #              'to query for images with specific attributes in '
-    #              'a file system.'),
-    # license='MIT',
-    # keywords='image duplicate search detection',
-    # long_description=read('README.md'),
-    # classifiers=[
-    #     "Development Status :: 2 - Pre-Alpha",
-    #     "Environment :: Console",
-    #     "License :: OSI Approved :: MIT License",
-    #     "Natural Language :: English"
-    # ],
-    # # package_dir = {'': 'imagedetection'},
-    # py_modules=['imagedetection'],
-    # # packages=['imagedetection', 'imagedetection.index', 'tests'],
-    # # packages=find_packages(),
+    description=('A module that can be imported and used via the command line '
+                 'to query for images with specific attributes in '
+                 'a file system.'),
+    license='MIT',
+    keywords='image duplicate search detection',
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English"
+    ],
     packages=['imagesearch', 'tests'],
     install_requires=[
         'click',
@@ -40,9 +35,5 @@ setup(
         'console_scripts': [
             'imagesearch = imagesearch.__main__:main'
         ]
-    },
-    # # entry_points='''
-    # #     [console_scripts]
-    # #     callimagedetection=imagedetection.index:cli
-    # # ''',
+    }
 )

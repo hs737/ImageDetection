da0c5a619bd7ce83dbcd761c5c99bad15286dc67
Harsh <hsinha146@gmail.com>
2016-12-30 10:13:14 -0500

    Recursive option working with both command line arguments

--- imagesearch/imagesearch.py
+++ imagesearch/imagesearch.py
@@ -3,36 +3,68 @@ import os
 import imagehash
 from PIL import Image
 
-def search(dir_path, query_path):
+def search(dir_path, recursive, query_path=None):
+    ext = ".jpg"
+    image_paths = []
+
+    for path, dirnames, files in os.walk(dir_path):
+        if not recursive:
+            dirnames[:] = dirnames[0]
+
+        image_paths += [os.path.join(path, filename) for filename in files
+                        if filename.lower().endswith(ext.lower())]
+
+    images_found = []
+    if query_path:
+        images_found = search_for_queried_image(image_paths, query_path)
+    else:
+        images_found = search_for_all_duplicates(image_paths)
+
+    output_images_found(images_found)
+
+def search_for_queried_image(image_paths, query_path):
+    query_image = Image.open(query_path)
+    query_hash = str(imagehash.dhash(query_image))
+    images_found = []
+
+    for image_path in image_paths:
+        print("Image path: {}".format(image_path))
+
+        image = Image.open(image_path)
+        image_hash = str(imagehash.dhash(image))
+
+        print("Image path: {}, Image hash: {}, Query hash: {}, EQ: {}".format(image_path, image_hash, query_hash, query_hash == image_hash))
+
+        if query_hash == image_hash:
+            print("File found: {}".format(image_path))
+            images_found.append([image_path])
+            # image = Image.open(image_path)
+            # image.show()
+
+    print("Images found: {}".format(images_found))
+    return images_found
+
+def search_for_all_duplicates(image_paths):
     db = {}
 
-    # loop over the image directory
-    print("Finger printing each discovered image")
-    image_paths = [x for x in glob.glob(os.path.join(dir_path, "*.jpg"))
-                   if not os.path.samefile(query_path, x)]
     for image_path in image_paths:
-        print("image path: {}".format(image_path))
-        # load the image and compute the difference hash
+        print("Image path: {}".format(image_path))
+
         image = Image.open(image_path)
-        h = str(imagehash.dhash(image))
+        image_hash = str(imagehash.dhash(image))
 
-        # extract the filename from the path and update the database
-        # filename = image_path[image_path.rfind("/") + 1:]
-        filename = os.path.basename(image_path)
-        print("Filename: {}".format(filename))
-        db[h] = (db[h] if h in db else []) + [filename]
+        db[image_hash] = (db[image_hash] if image_hash in db else []) + [image_path]
 
-    # load the query image, compute the difference image hash,
-    # and grab the images from the database that have the same hash
-    # value
-    print("Querying discovered finger prints")
-    query_image = Image.open(query_path)
-    h = str(imagehash.dhash(query_image))
-    filenames = db[h] if h in db else []
-    print("Found {} images: {}".format(len(filenames), filenames))
+    images_found = [db[image_hash] for image_hash in db if len(db[image_hash]) > 1]
+    print("Images found: {}".format(images_found))
 
-    # loop over the images
-    for filename in filenames:
-        print("File found: {}".format(filename))
-        image = Image.open(dir_path + "/" + filename)
-        image.show()
+    return images_found
+
+def output_images_found(images_found):
+    for image_list in images_found:
+        print("Image List found: {}".format(image_list))
+
+        # for image_path in image_list:
+        #     image = Image.open(image_path)
+        #     image.show()
+
--- imagesearch/index.py
+++ imagesearch/index.py
@@ -1,44 +1,38 @@
 # import the necessary packages
-# from PIL import Image
-# import imagehash
-# import glob
-import click
+from PIL import Image
+import imagehash
+import glob
 
-@click.command()
-def cli():
-    """Example script."""
-    click.echo("hahaha")
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required = True, help = "path to input dataset of images")
+ap.add_argument("-q", "--query", required = True, help = "path to the query image")
+args = vars(ap.parse_args())
+print("Arguments: {}".format(args))
 
-# # construct the argument parse and parse the arguments
-# ap = argparse.ArgumentParser()
-# ap.add_argument("-d", "--dataset", required = True, help = "path to input dataset of images")
-# ap.add_argument("-q", "--query", required = True, help = "path to the query image")
-# args = vars(ap.parse_args())
-# print("Arguments: {}".format(args))
+db = {}
 
-# db = {}
+# loop over the image dataset
+for imagePath in glob.glob(args["dataset"] + "/*.jpg"):
+    print("image path: {}".format(imagePath))
+    # load the image and compute the difference hash
+    image = Image.open(imagePath)
+    h = str(imagehash.dhash(image))
 
-# # loop over the image dataset
-# for imagePath in glob.glob(args["dataset"] + "/*.jpg"):
-#     print("image path: {}".format(imagePath))
-#     # load the image and compute the difference hash
-#     image = Image.open(imagePath)
-#     h = str(imagehash.dhash(image))
+    # extract the filename from the path and update the database
+    filename = imagePath[imagePath.rfind("/") + 1:]
+    db[h] = (db[h] if h in db else []) + [filename]
 
-#     # extract the filename from the path and update the database
-#     filename = imagePath[imagePath.rfind("/") + 1:]
-#     db[h] = (db[h] if h in db else []) + [filename]
+# load the query image, compute the difference image hash,
+# and grab the images from the database that have the same hash
+# value
+query = Image.open(args["query"])
+h = str(imagehash.dhash(query))
+filenames = db[h] if h in db else []
+print("Found {} images".format(len(filenames)))
 
-# # load the query image, compute the difference image hash,
-# # and grab the images from the database that have the same hash
-# # value
-# query = Image.open(args["query"])
-# h = str(imagehash.dhash(query))
-# filenames = db[h] if h in db else []
-# print("Found {} images".format(len(filenames)))
-
-# # loop over the images
-# for filename in filenames:
-#     print("File found: {}".format(filename))
-#     image = Image.open(args["dataset"] + "/" + filename)
-#     image.show()
+# loop over the images
+for filename in filenames:
+    print("File found: {}".format(filename))
+    image = Image.open(args["dataset"] + "/" + filename)
+    image.show()
